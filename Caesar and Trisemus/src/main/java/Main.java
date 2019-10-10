956904f529c89dc2d121ebef04d20d43c811d62d
Liz <eliz.com>
2019-10-10 23:10:39 +0300

    NewCode

--- Caesar and Trisemus/src/main/java/Main.java
+++ Caesar and Trisemus/src/main/java/Main.java
@@ -3,7 +3,7 @@ import util.CaesarEncryption;
 
 public class Main {
     public static void main(String[] args) {
-        String encrypt = CaesarEncryption.getEncryptionMessage("ABC", 3);
+        String encrypt = CaesarEncryption.getEncryptionMessage(":BaC", 3);
         System.out.println(encrypt);
     }
 }
--- Caesar and Trisemus/src/main/java/util/CaesarEncryption.java
+++ Caesar and Trisemus/src/main/java/util/CaesarEncryption.java
@@ -3,8 +3,29 @@ package util;
 public class CaesarEncryption {
     public static String getEncryptionMessage(String message, int k){
         String encryption = "";
+        int a = 0;
         for(int i = 0; i < message.length(); ++i){
-            encryption += (char)(message.charAt(i)+k);
+            a = message.charAt(i);
+            System.out.println(a);
+            if(a >= 65 && a <= 90){
+                a += k;
+                if(a > 90){
+                    a = 64 + (a - 90);
+                }
+                encryption += (char)a;
+            }
+            else {
+                if(a >= 97 && a <= 122){
+                    a += k;
+                    if(a > 122){
+                        a = 96 + (a - 122);
+                    }
+                    encryption += (char)a;
+                }
+                else{
+                    encryption += ' ';
+                }
+            }
         }
         return encryption;
     }
